e360a41faa7a775c06e2fa3ebd0e69571d36173a
Sharon Byegon <byegon.sharon@gmail.com>
2024-05-10 12:23:27 +0300

    completed

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -2,8 +2,11 @@ import { currentAgeForBirthYear } from "../utils";
 
 describe("currentAgeForBirthYear", () => {
   it("returns the age of a person based on the year of birth", () => {
-    const birthYear = 1984;
+    //Arrange: setting up any data that needs to be tested
+    const birthYear = 1986;
+    //Act: running the code that needs to be tested
     const ageOfPerson = currentAgeForBirthYear(birthYear);
+    //Assert: making claims about what we expect the outcome to be
     expect(ageOfPerson).toBe(38);
   });
 });
